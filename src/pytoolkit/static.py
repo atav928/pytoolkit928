475b48dc82a72ba84562d634c3c95feeb4c504ff
atav928 <31765990+atav928@users.noreply.github.com>
2023-10-02 14:11:25 -0400

    updated ca certificate logging

--- src/pytoolkit/static.py
+++ src/pytoolkit/static.py
@@ -5,3 +5,5 @@ DEFAULT_FROM: str = 'python-script@acme.com'
 DEFAULT_CC: list[str] = ['']
 DEFAULT_BCC: list[str] = ['']
 ENCODING: str = 'utf-8'
+TMP_PEM_POSTFIX = "_pytookit.pem"
+PEM_REGEX = r".*[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}_pytoolkit\.pem$"
