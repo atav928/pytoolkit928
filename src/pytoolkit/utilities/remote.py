619f8a2e0a075514991b66e615618d5dc90b47fb
atav928 <31765990+atav928@users.noreply.github.com>
2023-12-09 11:34:30 -0500

    Update remote.py
    Adds a base ssh remote command call directly that does not use auth keys

---
+++ src/pytoolkit/utilities/remote.py
@@ -0,0 +1,31 @@
+import pexpect
+import tempfile
+
+def ssh(host, cmd, user, password, timeout=30, bg_run=False):                                                                                                 
+    """SSH'es to a host using the supplied credentials and executes a command.                                                                                                 
+    Throws an exception if the command doesn't return 0.                                                                                                                       
+    bgrun: run command in the background"""                                                                                                                                    
+                                                                                                                                                                               
+    fname = tempfile.mktemp()                                                                                                                                                  
+    fout = open(fname, 'w')                                                                                                                                                    
+                                                                                                                                                                               
+    options = '-q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oPubkeyAuthentication=no'                                                                         
+    if bg_run:                                                                                                                                                         
+        options += ' -f'                                                                                                                                                       
+    ssh_cmd = 'ssh %s@%s %s "%s"' % (user, host, options, cmd)                                                                                                                 
+    child = pexpect.spawn(ssh_cmd, timeout=timeout)  #spawnu for Python 3                                                                                                                          
+    child.expect(['[pP]assword: '])                                                                                                                                                                                                                                                                                               
+    child.sendline(password)                                                                                                                                                   
+    child.logfile = fout                                                                                                                                                       
+    child.expect(pexpect.EOF)                                                                                                                                                  
+    child.close()                                                                                                                                                              
+    fout.close()                                                                                                                                                               
+                                                                                                                                                                                                                                                                                                                        
+    fin = open(fname, 'r')                                                                                                                                                     
+    stdout = fin.read()                                                                                                                                                        
+    fin.close()                                                                                                                                                                
+                                                                                                                                                                               
+    if 0 != child.exitstatus:                                                                                                                                                  
+        raise Exception(stdout)                                                                                                                                                
+                                                                                                                                                                               
+    return stdout
