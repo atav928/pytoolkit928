6bd96a27fa3716daacf7e0378edf6eb1838f5072
atav928 <31765990+atav928@users.noreply.github.com>
2024-01-16 15:04:53 -0500

    updated splunk hec format upload to divide uploads into 100 events per update and return the response.

--- src/pytoolkit/py_splunk/splunk.py
+++ src/pytoolkit/py_splunk/splunk.py
@@ -3,11 +3,18 @@
 from collections import OrderedDict
 import datetime
 from typing import Any, Optional, Union
+import logging
 
 from dataclasses import dataclass
 
-from pytoolkit.utilities import BaseMonitor, NONETYPE
+import urllib3
+import requests
 
+from pytoolkit.static import SPLUNK_HEC_EVENTPATH
+from pytoolkit.utilities import BaseMonitor, NONETYPE
+from pytoolkit.utils import chunk, reformat_exception
+
+splunk_log = logging.getLogger(__name__)
 
 @dataclass
 class SplunkHecHeader(BaseMonitor):
@@ -73,3 +80,53 @@ def splunk_hec_format(
     hec_json["events"] = {**hec_json["events"], **kwargs}
     hec_json["events"] = dict(sorted(hec_json["events"].items()))
     return hec_json
+
+def splunk_hec_upload(
+    server: str,
+    token: str,
+    hec_data: list[dict[str,Any]],
+    timeout: float = 15.0,
+    verify: Union[str,bool] = True,
+    port: int = 8088,
+    chunk_size: int = 100,
+    log: Any = splunk_log
+):
+    """
+    Upload Splunk Data.
+
+    :param server: _description_
+    :type server: str
+    :param token: _description_
+    :type token: str
+    :param hec_data: _description_
+    :type hec_data: Any
+    :param verify: _description_
+    :type verify: str
+    :param port: _description_, defaults to 8088
+    :type port: int, optional
+    :param chunk_size: Set size to split up data into if too large;
+     hec_data must be a list of json entries, defaults to 100 (0 will indicate all)
+    :type chunk_size: int, optional
+    """
+    if not verify:
+        log.error(f'msg="SSL Verficiation is off recommended this be fixed"| {verify=}')
+        urllib3.disable_warnings  # pylint: disable=pointless-statement
+    url = f"https://{server}:{port}/{SPLUNK_HEC_EVENTPATH}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Splunk {token}",
+        "X-Splunk-Request-Channel": token,
+    }
+    chunk_data: list[list[Any]] =  chunk(hec_data,chunk_size) if (chunk_size > 0 and chunk_size < len(hec_data)) else [hec_data]
+    resp_list: list[dict[str,Any]] = []
+    for payload in chunk_data:
+        response = requests.post(
+            url, headers=headers, json=payload, verify=verify, timeout=timeout
+        )
+        resp_list.append({"status_code": response.status_code, "payload_len": len(payload), "message": response.json() if response.json() else ""})
+        try:
+            response.raise_for_status()
+        except Exception as err:
+            error = reformat_exception(err)
+            splunk_log.error(f'msg="Unable to upload datea to splunk server"| splunk_server={server}, {error=}')
+    return resp_list
